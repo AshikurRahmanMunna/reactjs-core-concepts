4c006ffb1a814abc9d93a59dba7c350dfd515a6b
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2022-03-17 11:47:33 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+const singer = {name:'Mr. Mahfuz', job: 'Singer'}
+const singer2 = {name:'Eva Rahman', job: 'Singer2'}
+
+const singerStyle = {
+  color: 'green',
+  backgroundColor: 'white',
+  borderRadius: '10px'
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
